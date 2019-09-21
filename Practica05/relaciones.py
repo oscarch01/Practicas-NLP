dd73fdab1134b587fa9531ca48e6ae65266e6753
Oscar Chacón <mechjefer@hotmail.com>
2019-09-21 12:41:45 -0500

    Actualización REGEX Practica 05
    Actualización REGEX Practica 05

--- Practica05/relaciones.py
+++ Practica05/relaciones.py
@@ -20,7 +20,7 @@ if len(sys.argv)>=2:
         if mark:
             abstractsPassed = abstractsPassed + line + "\n"
     
-    patron1 = re.compile(r"((la|el|los|las)\s([\w\s]+)\s(es|son)\s([\w\s]+))")
+    patron1 = re.compile(r"((la|el|los|las|un|unos|una|unas)\s([á-úü\w]+)\s(es el|es la|es más|es una|es un|es también una|es|son los|son las|son una|son un|fue un|son)\s([á-úü\w]+))")
     patron2 = re.compile(r"(([\w,]+)\s(tal como|por ejemplo|como por ejemplo|tal como:|así como|así como)\s([\w,\s]+))")
     patron3 = re.compile(r"(([\w\s]+)\s(forma parte (de|del))\s([\w\s]+))")
     patron4 = re.compile(r"(([\w\s]+)\s(perteneciente|pertenece|pertenecientes|pertenecen)\s([\w\s]+))")
@@ -31,38 +31,42 @@ if len(sys.argv)>=2:
     repatron4 = patron4.findall(abstractsPassed)
 
     nodes = []
-    aristas = []
+    edges = []
     for match in repatron1:
         if match[2] not in nodes:
             nodes.append(match[2])
-        if match[2] not in nodes:
-            nodes.append(match[4])
-        aristas.append((match[2],match[4]))
-        
-    for match in repatron2:
-        if match[1] not in nodes:
-            nodes.append(match[1])
-        if match[3] not in nodes:
-            nodes.append(match[3])
-        aristas.append((match[1],match[3]))
-
-    for match in repatron3:
-        if match[1] not in nodes:
-            nodes.append(match[1])
         if match[4] not in nodes:
             nodes.append(match[4])
-        aristas.append((match[1],match[4]))
+        edges.append((match[2],match[4]))
+        
+    # for match in repatron2:
+    #     if match[1] not in nodes:
+    #         nodes.append(match[1])
+    #     if match[3] not in nodes:
+    #         nodes.append(match[3])
+    #     aristas.append((match[1],match[3]))
+
+    # for match in repatron3:
+    #     if match[1] not in nodes:
+    #         nodes.append(match[1])
+    #     if match[4] not in nodes:
+    #         nodes.append(match[4])
+    #     aristas.append((match[1],match[4]))
             
-    for match in repatron4:
-        if match[1] not in nodes:
-            nodes.append(match[1])
-        if match[3] not in nodes:
-            nodes.append(match[3])
-        aristas.append((match[1],match[3]))
+    # for match in repatron4:
+    #     if match[1] not in nodes:
+    #         nodes.append(match[1])
+    #     if match[3] not in nodes:
+    #         nodes.append(match[3])
+    #     aristas.append((match[1],match[3]))
     
+    print(nodes)
+    print("---------")
+    print(edges)
+
     G = nx.DiGraph()    
     G.add_nodes_from(nodes)
-    G.add_edges_from(aristas)
+    G.add_edges_from(edges)
     pos=nx.spring_layout(G)
     nx.draw_networkx_labels(G, pos, labels=dict([(i,i) for i in nodes]))
     nx.draw(G,pos)
